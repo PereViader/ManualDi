eef95437a07f99ce42b4d7957a17aa5f0ff5ad68
Pere Viader <pere.viader22@gmail.com>
2024-07-18 00:50:49 +0200

    Add FromDefault source generator

---
+++ ManualDi.Main.Generators/FromDefaultSourceGenerator.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ManualDi.Main.Generators
+{
+    [Generator]
+    public class FromDefaultSourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (context.SyntaxReceiver is not SyntaxReceiver receiver)
+                return;
+
+            var methods = new List<string>();
+
+            foreach (var classDeclaration in receiver.CandidateClasses)
+            {
+                var model = context.Compilation.GetSemanticModel(classDeclaration.SyntaxTree);
+                var classSymbol = model.GetDeclaredSymbol(classDeclaration) as INamedTypeSymbol;
+
+                if (classSymbol is null || classSymbol.DeclaredAccessibility != Accessibility.Public)
+                    continue;
+
+                var publicConstructors = classSymbol.Constructors.Where(c => c.DeclaredAccessibility == Accessibility.Public).ToList();
+
+                if (publicConstructors.Count != 1)
+                    continue;
+
+                var constructor = publicConstructors.Single();
+                var arguments = string.Join(", ", constructor.Parameters.Select(p => $"c.Resolve<{FullyQualifyType(p.Type)}>()"));
+                var className = FullyQualifyType(classSymbol);
+
+                var methodSource = $@"
+        public static TypeBinding<{className}, {className}> FromDefault(
+            this TypeBinding<{className}, {className}> typeBinding
+            )
+        {{
+            typeBinding.FromMethod(static c => new {className}(
+                {arguments}
+                ));
+            return typeBinding;
+        }}
+";
+
+                methods.Add(methodSource);
+            }
+
+            var methodsSource = string.Join("", methods);
+
+            var formattedAssemblyName = Regex.Replace(context.Compilation.AssemblyName ?? string.Empty, @"[^A-Za-z0-9_]", "");
+            var source = $@"
+using ManualDi.Main;
+
+namespace ManualDi.Main
+{{
+    public static class ManualDiFromDefaultExtensions_{formattedAssemblyName}
+    {{
+{methodsSource}
+    }}
+}}
+";
+
+            context.AddSource($"ManualDiFromDefaultExtensions.g.cs", SourceText.From(source, Encoding.UTF8));
+        }
+
+        private static string FullyQualifyType(ITypeSymbol typeSymbol)
+        {
+            return typeSymbol.ContainingNamespace.IsGlobalNamespace ? typeSymbol.Name : $"{typeSymbol.ContainingNamespace}.{typeSymbol.Name}";
+        }
+
+        private class SyntaxReceiver : ISyntaxReceiver
+        {
+            public List<ClassDeclarationSyntax> CandidateClasses { get; } = new List<ClassDeclarationSyntax>();
+
+            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+            {
+                if (syntaxNode is ClassDeclarationSyntax classDeclaration)
+                {
+                    CandidateClasses.Add(classDeclaration);
+                }
+            }
+        }
+    }
+}
---
+++ ManualDi.Main.Tests/TestFromDefaultSourceGenerator.cs
@@ -0,0 +1,78 @@
+﻿using ManualDi.Main.Generators;
+
+namespace ManualDi.Main.Tests;
+
+using System.Linq;
+using NUnit.Framework;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+public class FromDefaultSourceGeneratorTests
+{
+    [Test]
+    public void TestFromDefaultSourceGenerator()
+    {
+        var code = @"
+namespace SomeNamespace; //We are testing that it properly picks up namespaces
+
+public class MyClass
+{
+    public MyClass(OtherClass otherClass) { }
+}
+
+public class OtherClass
+{
+}
+";
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(SourceText.From(code, Encoding.UTF8));
+        var compilation = CSharpCompilation.Create("Test Assembly-34#",
+            new[] { syntaxTree },
+            new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) },
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        var generator = new FromDefaultSourceGenerator();
+        var driver = CSharpGeneratorDriver.Create(generator);
+        driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out _);
+
+        var generatedTrees = outputCompilation.SyntaxTrees.ToList();
+        Assert.AreEqual(2, generatedTrees.Count); // Original + generated
+
+        var generatedCode = generatedTrees.Skip(1).First().ToString();
+        var expectedCode = @"
+using ManualDi.Main;
+
+namespace ManualDi.Main
+{
+    public static class ManualDiFromDefaultExtensions_TestAssembly34
+    {
+
+        public static TypeBinding<SomeNamespace.MyClass, SomeNamespace.MyClass> FromDefault(
+            this TypeBinding<SomeNamespace.MyClass, SomeNamespace.MyClass> typeBinding
+            )
+        {
+            typeBinding.FromMethod(static c => new SomeNamespace.MyClass(
+                c.Resolve<SomeNamespace.OtherClass>()
+                ));
+            return typeBinding;
+        }
+
+        public static TypeBinding<SomeNamespace.OtherClass, SomeNamespace.OtherClass> FromDefault(
+            this TypeBinding<SomeNamespace.OtherClass, SomeNamespace.OtherClass> typeBinding
+            )
+        {
+            typeBinding.FromMethod(static c => new SomeNamespace.OtherClass(
+                
+                ));
+            return typeBinding;
+        }
+
+    }
+}
+";
+        
+        Assert.AreEqual(expectedCode.Trim(), generatedCode.Trim());
+    }
+}

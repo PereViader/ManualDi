5ff5b37ba2779d15281acb033447345aeaf700f9
Pere Viader <pere.viader22@gmail.com>
2025-03-22 22:02:50 +0100

    Add documentation

--- ManualDi.Main/ManualDi.Main.Tests/TestDiContainerCyclic.cs
+++ ManualDi.Main/ManualDi.Main.Tests/TestDiContainerCyclic.cs
@@ -13,6 +13,14 @@ public class TestDiContainerCyclic
         public void Inject(A _) { }
     }
     
+    /// <summary>
+    /// In this test, notice that B depends on A and A depends on B
+    /// This is a cyclic dependency
+    /// Because this is impossible to create, notice that B does not inject A on the constructor
+    /// Instead it does it on a separate inject method
+    /// This test should fail when the container is built but for some reason the dependencies are not
+    /// created in the proper order
+    /// </summary>
     [Test]
     public async Task TestCyclic()
     {

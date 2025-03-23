54b0e6697fc7af6543afbdd8633909fba39d55df
Pere Viader <pere.viader22@gmail.com>
2025-03-23 21:39:45 +0100

    Improve unity3d workflow when updating the native c# part

--- ManualDi.Unity3d/GenerateUnityPackage/GenerateUnity3dPackage.sh
+++ ManualDi.Unity3d/GenerateUnityPackage/GenerateUnity3dPackage.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cp -r "ManualDi.Unity3d/Assets/ManualDi.Unity3d/." "UnityPackageRelease/ManualDi.Unity3d/"
-cp "ManualDi.Unity3d/GenerateUnityPackage/ManualDi.Main.Generators.dll.meta" "UnityPackageRelease/ManualDi.Main.Generators.dll.meta"
 mv "UnityPackageRelease/ManualDi.Unity3d/Samples" "UnityPackageRelease/ManualDi.Unity3d/Samples~"
 rm "UnityPackageRelease/ManualDi.Unity3d/Samples.meta"
